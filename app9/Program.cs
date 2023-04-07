1f74513aec49408a035c8f445717bd091843bef4
berkay8987 <65907075+berkay8987@users.noreply.github.com>
2023-04-07 11:03:55 +0300

    Add files via upload

---
+++ app9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Serilog;
+
+namespace app9
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create Logger
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Information("This program tries to divide a number with 0.");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Something went wrong");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+}
