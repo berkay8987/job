650f6f0a7715c23037969ae46b58067bc9677930
berkay8987 <65907075+berkay8987@users.noreply.github.com>
2023-04-14 16:34:14 +0300

    Add files via upload

---
+++ 1404/SqlConnectionExamples2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlConnectionExamples2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DataTable kullanarak verileri yazmak.
+            string connectionString = "Data Source=NB320882;Initial Catalog=NORTHWND;User ID=sa;Password=5xgkW8RUZr";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT TOP 3 ContactName FROM Customers";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                DataTable table = new DataTable();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(table);
+
+                foreach (DataRow row in table.Rows)
+                {
+                    foreach (DataColumn column in table.Columns)
+                    {
+                        Console.Write(row[column] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ 1404/SqlConnectionExamples3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlConnectionExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=NB320882;Initial Catalog=NORTHWND;User ID=sa;Password=5xgkW8RUZr";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT EmployeeID, ShipName, ShipCountry FROM Orders";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}\t{1}\t\t\t{2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+                }
+            }
+        }
+    }
+}
